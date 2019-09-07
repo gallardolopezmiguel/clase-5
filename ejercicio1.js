ba7e01574bf55d3c19dd500ff38299988ee9a942
gallardolopezmiguel <miguel.lopez.gallar@gmail.com>
2019-09-06 21:59:50 -0500

    Primera

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+
+
+
+
+const hambre = (prompt("Tienes Hambre???"));
+
+if (hambre == "no") {
+    console.log("No tengo hambre");
+} else {
+   console.log ("estoy triste tengo hambre");
+}
+
+
+
+
